3234ffbbb616087ae5ec2e8c2d84929175673e7a
Michael L Perry <michael@qedcode.com>
2013-12-26 17:18:05 -0600

    Initial commit.

---
+++ Multinotes.PhoneApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Multinotes.PhoneApp.Resources;
+
+namespace Multinotes.PhoneApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
