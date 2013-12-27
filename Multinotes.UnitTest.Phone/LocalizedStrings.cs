b474e69cd0c8124b420557027d8d736ebc887570
Michael L Perry <michael@qedcode.com>
2013-12-27 11:07:58 -0600

    Added phone test project.

---
+++ Multinotes.UnitTest.Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Multinotes.UnitTest.Phone.Resources;
+
+namespace Multinotes.UnitTest.Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
