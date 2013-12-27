2afbb9243bab4792a92db3147242898e098f274e
Michael L Perry <michael@qedcode.com>
2013-12-27 15:34:07 -0600

    Added web projects.

---
+++ Multinotes.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Multinotes.Web.Startup))]
+namespace Multinotes.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
